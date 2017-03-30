854b5740e238ed18a26d940737f80df51aac0cd7
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2017-03-30 16:41:05 +0200

    Added derivative, integral still bugs the program out.

--- main.c
+++ main.c
@@ -13,11 +13,13 @@ short CBlack = 22;
 short CWhite = 63;
 short COffset = -1;
 
-short Kp = 1;
-short Kd = 3;
+short Kp = 10;
+short Kd = 100;
 short Tp = 25;
 
 short lastError = 0;
+short integral = 0;
+
 void init()
 {
 	BWOffset = (BWWhite + BWBlack) / 2;
@@ -33,15 +35,16 @@ task main()
 	while(1)
 	{
 		short error = errorAmount(sensorValue[BWSensor]); //First we calculate the position based on our sensors.
-		float Turn = (Kp * error + Kd * (error - lastError));
-		lastError = error;
-		int rightSpeed = Tp - Turn;
-		int leftSpeed = Tp + Turn;
+		short derivative = error - lastError;
+		short Turn = (Kp * error + Kd * derivative) / 10;
+		short rightSpeed = Tp - Turn;
+		short leftSpeed = Tp + Turn;
 		if (rightSpeed > maxSpeed) rightspeed = maxSpeed;
 		else if (rightSpeed < 0) rightSpeed = 0;
 		if (leftSpeed > maxSpeed) leftSpeed = maxSpeed;
 		else if (leftSpeed < 0) leftSpeed = 0;
 		motor[motorB] = rightSpeed;
 		motor[motorC] = leftSpeed;
+		lastError = error;
 	}
 }
