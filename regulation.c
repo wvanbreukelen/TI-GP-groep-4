d9d64232d56ea5f8520764183a92fad8f85b3b55
Tonyvdk <t.r.vanderkrogt@gmail.com>
2017-04-03 13:37:28 +0200

    Acceleration and deceleration functions
    This is for a fluently startup and stop for the motors so that a LEGO
    man doesn't fall off

---
+++ regulation.c
@@ -0,0 +1,49 @@
+/**
+ml = Left motor
+mr = Right motor
+speed = What the speed is
+
+return = new speed after the function
+**/
+short Acceleration(short ml, short mr, short speed){
+	while (speed<=25){
+		speed++;
+
+		motor[motorB]=speed;
+		motor[motorC]=speed;
+		wait1Msec(50);
+
+	}
+	return speed;
+}
+
+/**
+ml = Left motor
+mr = Right motor
+speed = What the speed is
+
+return = new speed after the function
+**/
+short Deceleration(short ml, short mr, short speed){
+		while(speed>=0){
+			speed--;
+
+			motor[motorB]=speed;
+			motor[motorC]=speed;
+			wait1Msec(50);
+		}
+		return speed;
+}
+/*
+small test for acceleration and deceleration
+*/
+task main()
+{
+	short currentspeed=0;
+
+	currentspeed=Acceleration(motorB,motorC,currentspeed);
+	wait1Msec(1000);
+	Deceleration(motorB,motorC,currentspeed);
+
+
+}
