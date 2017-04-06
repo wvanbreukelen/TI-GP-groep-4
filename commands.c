6b5c36d76eadd756379103a77579974245f9e33a
Wiebe van Breukelen <wiebe@vanbreukelen.me>
2017-04-06 14:48:33 +0200

    Major changes, controlling robot with smartphone is now possible

--- commands.c
+++ commands.c
@@ -13,7 +13,20 @@ int readBluetoothData(ubyte* buffer, int nMaxBufferSize)
   return sizeOfMessage;
 }
 
-void robotTurn(short deg, bool inverted = false){
+void robotTurn(short m, short deg){
+	nMotorEncoder[m] = 0;
+  nMotorEncoderTarget[m] = deg;
+  int power = 25;
+  if (deg < 0)
+  {
+  	power *= -1;
+  }
+
+  motor[m] = power;
+  while(nMotorRunState(m) != runStateIdle){}
+}
+
+/**void robotTurn(short deg, bool inverted = false){
   nSyncedMotors = (inverted) ? synchBC : synchCB;
   nSyncedTurnRatio = -100;
 
@@ -33,16 +46,24 @@ void robotTurn(short deg, bool inverted = false){
 	}
 
 
-}
+}**/
 
 void robotTurnLeft()
 {
-	robotTurn(180);
+	robotTurn(motorB, 370);
 }
 
 void robotTurnRight()
 {
-	robotTurn(180, true);
+	robotTurn(motorC, 370);
+}
+
+void robotTurnAround()
+{
+	nSyncedMotors = synchCB;
+	nSyncedTurnRatio = -100;
+	robotTurn(motorC, 360);
+	nSyncedMotors = synchNone;
 }
 
 ubyte* waitForInput()
@@ -62,29 +83,46 @@ ubyte* waitForInput()
 
 bool handleInput(ubyte* input)
 {
+	startTask(soundCrossingTask);
+
+	clearDisplay();
+	displayTextLine(4, "%#X", *input);
+
 	switch (*input)
 	{
 		case 0x4C:
 			// Turn left
-			stopTask(startPID);
 			stopAllMotors();
 			robotTurnLeft();
 
 			break;
 		case 0x52:
 			// Turn right
-			stopTask(startPID);
+			stopAllMotors();
 			robotTurnRight();
 
 			break;
+		case 0x55:
+			// Go forward
+			motor[motorB] = 25;
+			motor[motorC] = 25;
+
+			while (onCrossRoads(SensorValue[BWSensor], SensorValue[CSensor])) {}
+
+			break;
 		case 0x46:
+			// Fire, stop the robot
 			stopAllTasks();
 			stopAllMotors();
 
 			break;
+		case 0x44:
+			// Turn around
+			stopAllMotors();
+			robotTurnAround();
+			break;
 		default:
 			return false;
-			//startTask(soundErrorTask);
 	}
 
 	return true;
@@ -92,31 +130,65 @@ bool handleInput(ubyte* input)
 
 task commandHandlerTask()
 {
-	ubyte* input = waitForInput();
+	//ubyte* input = waitForInput();
 
-	if (!handleInput(input))
-	{
-		// Command not found
-		startTask(soundErrorTask);
-	}
-
-}
-
-task handleStopCmd()
-{
 	ubyte nRcvBuffer[kMaxSizeOfMessage];
 
 	while (true)
 	{
 		if (readBluetoothData(nRcvBuffer, kMaxSizeOfMessage) > 0)
 		{
-			if (nRcvBuffer == 0x46)
-			{
-				stopAllTasks();
-			}
+			stopTask(handleCrossroads);
+			stopTask(startPID);
+
+			if (!handleInput(nRcvBuffer)) startTask(soundErrorTask);
+
+			startTask(startPID);
+			startTask(handleCrossroads);
 		}
+
+		wait1Msec(200);
 	}
+
+		// Stop PID and crossroad tasks
+		//stopTask(startPID);
+	//stopTask(handleCrossroads);
+
+	//startTask(soundCrossingTask);
+
+	//if (!handleInput(input))
+	//{
+		// Cannot handle this kind of input, play an error tune
+		//startTask(soundErrorTask);
+	//}
+
+		// Re-enable PID and crossroad tasks
+	//startTask(startPID);
+	//startTask(handleCrossroads);
+
 }
+
+/**void handleUserDecision(ubyte* buffer)
+{
+	switch (*buffer)
+	{
+		case 0x46:
+			// Fire
+			stopAllTasks();
+			break;
+		case 0x4C:
+			// Left
+			break;
+		case 0x52:
+			// Right
+			break;
+		case 0x44:
+			// Down
+			break;
+		default:
+			return;
+	}
+}**/
 
 // Some sample code, remove it later @wiebe
 
