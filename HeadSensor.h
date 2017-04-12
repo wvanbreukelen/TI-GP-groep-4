a5dbd9b6cca573cf77ca9a582d3e43643ccee72e
Tonyvdk <t.r.vanderkrogt@gmail.com>
2017-04-12 11:36:58 +0200

    Header file HeadSensor

---
+++ HeadSensor.h
@@ -0,0 +1,14 @@
+//
+// Created by trvan on 12-4-2017.
+//
+
+#ifndef HEADSENSOR_H
+#define HEADSENSOR_H
+
+bool isWall(short minDistCm = 25);
+void newEvade();
+void avoid(int whereTo);
+void avoid(int whereTo);
+
+
+#endif //HEADSENSOR_H
